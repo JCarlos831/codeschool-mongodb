9354d42dc68dd02fcffb19dea503d2b635cc5cdb
Juan Montoya <jcmontoya@gmail.com>
2018-02-02 01:08:18 +0000

    Initial Commit

---
+++ mongo.js
@@ -0,0 +1,13 @@
+var monsterFunction = function(name, health, last_fought, attacks, stats, db){
+    
+};
+
+db.monsters.insert();
+
+console.log(db.monsters.find());
+
+db.monsters.find({"name": "The Vicious Frost Dog"});
+
+db.monsters.find({"attacks": "bite"});
+
+db.monsters.find({"stats.defense": 10});
